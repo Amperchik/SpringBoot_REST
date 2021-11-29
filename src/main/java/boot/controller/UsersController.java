0fa83fbdeef4b8c4e3ed78d168b687c933ce1413
Amperchik <aleksejjrdnv@rambler.ru>
2021-11-29 16:31:12 +0300

    Initial commit

---
+++ src/main/java/boot/controller/UsersController.java
@@ -0,0 +1,75 @@
+package boot.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import boot.model.User;
+import boot.service.UserService;
+import java.security.Principal;
+
+@Controller
+public class UsersController {
+    private final UserService userService;
+
+    public UsersController(UserService userService) {
+        this.userService = userService;
+    }
+
+    /**
+     * Метод контроллера который выводит список всех user с БД .
+     * В возвращаемом значение название View (html страницы).
+     * Аннотация указывает приложению что это get запрос HTTP
+     */
+    @GetMapping(value = "/admin")
+    public String index(Model model) {
+        model.addAttribute("users", userService.getUsers());
+        return "index";
+    }
+
+    /**
+     * Метод контроллера который возвращает пользователя из БД по id.
+     * Параемтр переданный в строке URL извлекается с помощью аннотации @PathVariable
+     * и присваивается переменной id.
+     */
+    @GetMapping("/admin/create")
+    public String createForm(User user, Model model) {
+        model.addAttribute(user);
+        return "create";
+    }
+
+    @PostMapping("/admin/create")
+    public String create(User user, String[] role) {
+        user.setRoles(role);
+        userService.addUser(user);
+        return "redirect:/admin";
+    }
+
+    @GetMapping("/admin/delete")
+    public String deleteById(@RequestParam long id) {
+        userService.deleteById(id);
+        return "redirect:/admin";
+    }
+
+    @GetMapping("/admin/update")
+    public String updateUserForm(@RequestParam long id, Model model) {
+        User user = userService.getUserId(id);
+        model.addAttribute("user", user);
+        model.addAttribute("roles", user.getRoles());
+        return "update";
+    }
+
+    @PostMapping("/admin/update")
+    public String updateUser(User user, String[] role) {
+        user.setRoles(role);
+        userService.updateUser(user);
+        return "redirect:/admin";
+    }
+
+    @GetMapping("/user")
+    public String test(Model model, Principal user) {
+        model.addAttribute("user", userService.findByLastName(user.getName()));
+        return "/user";
+    }
+}
---
+++ src/main/java/boot/security/SecurityConfig.java
@@ -0,0 +1,54 @@
+package boot.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+@ComponentScan({"test.test.service", "test.test.dao", "test.test.model", "test.test.controller","test.test.security"})
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+   private final UserDetailsService userDetailsService;
+   private final SuccessUserHandler successUserHandler;
+
+@Autowired
+    public SecurityConfig(@Qualifier("userServiceImpl") UserDetailsService userDetailsService, SuccessUserHandler successUserHandler) {
+        this.userDetailsService = userDetailsService;
+        this.successUserHandler = successUserHandler;
+    }
+
+    @Bean
+    public static NoOpPasswordEncoder passwordEncoder() {
+        return (NoOpPasswordEncoder) NoOpPasswordEncoder.getInstance();
+    }
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService);
+        authProvider.setPasswordEncoder(passwordEncoder());
+        return authProvider;
+    }
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth)  {
+        auth.authenticationProvider(authenticationProvider());
+    }
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        // http.csrf().disable(); - попробуйте выяснить сами, что это даёт
+        http.authorizeRequests()
+                .antMatchers("/").authenticated()// доступность всем
+                .antMatchers("/user").hasAnyRole("USER","ADMIN")// разрешаем входить на /user пользователям с ролью User
+                .antMatchers("/admin/**").hasRole("ADMIN")
+                .and().formLogin()  // Spring сам подставит свою логин форму
+                .successHandler(successUserHandler); // подключаем наш SuccessHandler для перенеправления по ролям
+    }
+}
